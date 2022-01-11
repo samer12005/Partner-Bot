228aef28b71eed4eda40b1f294164182b7b91668
REDAxDevV <88611334+REDAxDevV@users.noreply.github.com>
2022-01-11 10:03:17 +0300

    Update and rename Ano.js to Reda.js

--- Reda.js
+++ Reda.js
@@ -51,7 +51,7 @@ ${message.content}`)
 client.on('ready', () => {
 console.log(`ON ${client.guilds.size} Servers`);
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`PARTNER ANO`) 
+client.user.setGame(`PARTNER Reda`) 
 });
 
 
